cdc403e92190896a7c0267fb01fbb58672a4f3f3
Wahyu Pratama <wahyupraunsri@gmail.com>
2025-02-06 10:32:13 +0700

    feat: navbar

---
+++ apps/web/src/app/(homepage)/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "../../components/navbar";
+import PlaceHolder from "../../components/placeholder";
+import Footer from "../../components/footer";
+import Foot from "../../components/foote";
+const HomePage = () => {
+  return (
+    <div>
+        <Navbar />
+      <h1>Home Page</h1>
+      <PlaceHolder />
+      <Footer />
+      <Foot />
+    </div>
+  );
+};
+export default HomePage;
